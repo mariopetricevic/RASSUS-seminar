ce06edcaa8db139053bc80852185fd2ab6d3fb74
mariopetricevic <mario.petricevic22@gmail.com>
2021-01-06 15:59:54 +0100

    izmjene

--- microservices/aggregator-microservice/src/main/java/hr/fer/rassus/mikrousluge/aggregatormicroservice/AggregatorController.java
+++ microservices/aggregator-microservice/src/main/java/hr/fer/rassus/mikrousluge/aggregatormicroservice/AggregatorController.java
@@ -4,6 +4,7 @@ package hr.fer.rassus.mikrousluge.aggregatormicroservice;
 //import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,10 @@ public class AggregatorController {
 	@Autowired
 	private RetrofitImplementation service;
 	
+	@Value("${my.greeting}")
+	private String mjernajedinica;
+	
+	
 	
 	@GetMapping("/readings")
 	public String getReadings()
@@ -20,7 +25,7 @@ public class AggregatorController {
 		//Map<String,String> map=new HashMap<>();
 		String temperature="Temperature: "+ service.getTemperature().getTemperature().toString();
 		String humidity="Humidity: "+ service.getHumidity().getHumidity().toString();
-		String reading=temperature+ "<br>"+ humidity+"%";
+		String reading=temperature+ "<br>"+ humidity+"%" + mjernajedinica;
 		//map.put(service.getTemperature().getTemperature(), service.getHumidity().getHumidity());
 		//map.put(temperature, humidity);
 		//return map;
